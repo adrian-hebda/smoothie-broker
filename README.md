959177a06a26d03e43b311fab0ced1dcfbdca074
Tituu <adrianhebda2@gmail.com>
2023-04-16 18:56:54 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+This project is a simple application for stock market simulation.
+
+The main goal is to create fully functional game based on stock market. 
+Players will be able to compete between each other in daily, weekly, monthly 
+or even yearly events. But before it happens it is still simple side project.
---
+++ src/main/java/com/smoothie/broker/SmoothieBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.smoothie.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothieBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmoothieBrokerApplication.class, args);
+    }
+
+}
